2da3238e643bc6efed01fde86727a215b13235e6
Aayush Yadav <yadavaayush002@gmail.com>
2023-04-03 22:38:13 +0530

    File based routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      This is the about page of the app
+    </div>
+  )
+}
+
+export default about
---
+++ pages/blogPost/keyboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const keyboard = () => {
+  return (
+    <div>
+      This is a new route page - How to use keyboard
+    </div>
+  )
+}
+
+export default keyboard
